06c46860892f2fd33d7efc39dbeab60519f28180
NeyoCreator <neos25722@gmail.com>
2022-11-16 15:15:54 +0200

    Added check_all_messages function

--- main.py
+++ main.py
@@ -13,6 +13,27 @@ def message_probability(user_message,recognised_words,single_response=False, req
     #CALCULATE THE PERCENTAGE OF RECOGNISED WORDS IN A USER MESSAGE 
     percentage = float(message_certantity)/ float(len(recognised_words))
 
+    #CHECKS THAT THE REQUIRED WORDS ARE IN THE STRING
+    for word in required_words:
+        if word not in user_message:
+            has_required_words = False
+            break
+
+    if has_required_words or single_response:
+        return int(percentage*100)
+    else :
+        return 0
+
+
+def check_all_messages(message):
+    high_prob_list = {}
+
+    def response(bot_response,list_of_words, single_response = False, required_words=[]):
+        nonlocal high_prob_list
+        high_prob_list[bot_response]=message_probability(message, list_of_words, single_response,required_words)
+
+
+
 
 
 #CREATE FUNCTION
