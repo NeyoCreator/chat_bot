e38cb9570bc5c9da1229c88599745298a1981633
tsndlov022 <tsndlov022@student.wethinkco.de>
2022-12-07 05:11:47 +0200

    Finilised API creation

--- 2.application/backend/controllers/workoutController.js
+++ 2.application/backend/controllers/workoutController.js
@@ -1,11 +1,28 @@
 const Workout = require('../models/workoutModel')
+const mongoose = require('mongoose')
 
 //1.GET ALL WORKOUTS
-const getWorkouts = async (res,req)=>{
-    res.json({mssg: 'GET ALL WORKOUTS'})    
+const getWorkouts = async (req,res)=>{
+    const workouts =await Workout.find({}).sort({created:-1})
+
+    res.status(200).json(workouts)
 }
 
 //2.GET SINGLE WORKOUTS
+const getWorkout = async (req,res) =>{
+    const { id } = req.params
+    const workout = await Workout.findById(id)
+
+    if(!mongoose.Types.ObjectId.isValid(id)){
+        return res.status(404).json({error : 'NO SUCH WORKOUT'})
+    }
+    
+    if (!workout){
+        return res.status(404).json({error: 'No such workout'})
+    }
+
+    res.status(200).json(workout)
+}
 
 //3.CREATE NEW WORKOUT 
 const createWorkOut = async (req,res) => {
@@ -22,9 +39,43 @@ const createWorkOut = async (req,res) => {
 } 
 
 //DELETE WORKOUT 
+const deleteWorkout = async(req,res) => {
+    const {id} = req.params
+    if(!mongoose.Types.ObjectId.isValid(id)){
+        return res.status(404).json({error : 'NO SUCH WORKOUT'})
+    }
+
+    const workout = await Workout.findOneAndDelete({_id:id})
+
+    if (!workout){
+        return res.status(400).json({error: 'No such workout'})
+    }
+
+    res.status(200).json(workout)
+}
 
 //UPDATE WORKOUT 
+const updateWorkOut = async(req,res) => {
+    const {id} = req.params
+    if(!mongoose.Types.ObjectId.isValid(id)){
+        return res.status(404).json({error : 'NO SUCH WORKOUT'})
+    }
+
+    const workout = await Workout.findOneAndUpdate({_id:id}, {
+        ...req.body
+    })
+
+    if (!workout){
+        return res.status(400).json({error: 'No such workout'})
+    }
+
+    res.status(200).json(workout)
+}
 
 module.exports = {
-    createWorkOut
+    createWorkOut,
+    getWorkout,
+    getWorkouts,
+    deleteWorkout,
+    updateWorkOut
 }
--- 2.application/backend/routes/workouts.js
+++ 2.application/backend/routes/workouts.js
@@ -2,31 +2,27 @@ const express =require('express')
 const Workout = require('../models/workoutModel')
 const router =express.Router()
 const {
-    createWorkOut
+    getWorkouts,
+    getWorkout,
+    createWorkOut,
+    deleteWorkout,
+    updateWorkOut
 } = require('../controllers/workoutController')
 
 
 //1.GET WORKOUTS
-router.get('/', (eq,res) => {
-    res.json({mssg: 'GET ALL WORKOUTS'})
-})
+router.get('/',getWorkouts)
 
 //2.GET SINGLE WOLKOUT
-router.get('/:id',(req,res) =>{
-    res.json({mssg: 'GET single workout'})
-})
+router.get('/:id',getWorkout)
 
 //3.POST NEW WORKOUT
 router.post('/',createWorkOut)
 
 //4.DELETE WORKOUT
-router.delete('/:id',(req,res)=>{
-    res.json({mssg: 'DELETE A WORKOUT'})
-})
+router.delete('/:id',deleteWorkout)
 
 //5.UPDATE WORKOUT
-router.patch('/:id',(eq,res)=>{
-    res.json({mssg: 'UPDATE A WORKOUT'})
-})
+router.patch('/:id',updateWorkOut)
 
 module.exports = router
