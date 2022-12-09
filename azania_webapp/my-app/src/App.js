81823a6d0ee7a7125d6059b08a0f83755be82099
tsndlov022 <tsndlov022@student.wethinkco.de>
2022-12-09 08:26:45 +0200

    Added azania webapp

---
+++ azania_webapp/my-app/src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Battery Status: 40%</h1>
+      <br></br>
+      <h1>Next Schedule: 20:30</h1>
+
+    </div>
+  );
+}
+
+export default App;
