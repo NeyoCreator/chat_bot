ed13258007da469c7bc6d182503c24ad212438ab
tsndlov022 <tsndlov022@student.wethinkco.de>
2022-12-09 09:02:32 +0200

    Chaged spelling

--- azania_webapp/my-app/src/App.js
+++ azania_webapp/my-app/src/App.js
@@ -4,7 +4,7 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <h1>Battery Status:40%</h1>
+      <h1>Battery Status:40% is</h1>
       <br></br>
       <h1>Next Schedule: 20:30</h1>
 
