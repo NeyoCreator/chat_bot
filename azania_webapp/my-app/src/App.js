feeab13f871caf6bb6c56b3ddd6bfb38c1153128
tsndlov022 <tsndlov022@student.wethinkco.de>
2022-12-09 11:03:07 +0200

    created first component

--- azania_webapp/my-app/src/App.js
+++ azania_webapp/my-app/src/App.js
@@ -1,12 +1,17 @@
 import logo from './logo.svg';
+import BasicCard from './card';
 import './App.css';
+
 
 function App() {
   return (
     <div className="App">
-      <h1>Battery Status:40% is</h1>
+      <h1>Battery Status:40%</h1>
       <br></br>
       <h1>Next Schedule: 20:30</h1>
+
+      <center><BasicCard></BasicCard></center> 
+
 
     </div>
   );
