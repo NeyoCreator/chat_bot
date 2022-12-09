1c60d61d03e92fc10678b73b504cb6ced8ebb96e
tsndlov022 <tsndlov022@student.wethinkco.de>
2022-12-09 19:10:43 +0200

    added additionl header tag

--- azania_webapp/my-app/src/App.js
+++ azania_webapp/my-app/src/App.js
@@ -5,8 +5,10 @@ import Navbar from './components/Navbar';
 import Home from './components/Home'
 import './App.css';
 
-function App() {
 
+
+function App() {
+  
   return (
     <div className="App">
       <BrowserRouter>
--- azania_webapp/my-app/src/components/Home.js
+++ azania_webapp/my-app/src/components/Home.js
@@ -3,8 +3,10 @@ import BasicCard from "./card1"
 import BasicCard2 from "./card2"
 
 const Home = () => {
+  var x = 4
     return (
       <div className="Home">
+        <h1>hello {x}</h1>
         <br></br>
         <BasicCard></BasicCard>
         <br></br>
