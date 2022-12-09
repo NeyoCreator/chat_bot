03a9f6f2b11ed8d0a68c010f14ea798da9720a09
tsndlov022 <tsndlov022@student.wethinkco.de>
2022-12-09 08:09:12 +0200

    created react app and hosted on firebase

--- 2.application/frontend/src/pages/Home.js
+++ 2.application/frontend/src/pages/Home.js
@@ -1,23 +1,6 @@
-import { useEffect, useState } from "react"
+// import { useEffect, useState } from "react"
 
 const Home = () => {
-  const [workouts, setWorkOuts] = useState(null)
-
-  useEffect(() => {
-
-    const fetchWorkouts = async () => {
-      const response = await fetch('http://localhost:4000/api/workouts')
-      const json = await response.json()
-
-      if (response.ok) {
-        setWorkOuts(json)
-      }
-    }
-    
-    fetchWorkouts()
-
-  },[])
-
     return (
       <div className="home">
         <h2>we are looking</h2>
