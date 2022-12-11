0ab2143b7380b6226d2517da06ae83c085e1b0bf
tsndlov022 <tsndlov022@student.wethinkco.de>
2022-12-11 16:06:14 +0200

    Created backend and made simple api call

--- azania_webapp/my-app/src/components/Home.js
+++ azania_webapp/my-app/src/components/Home.js
@@ -2,9 +2,9 @@ import Navbar from "./Navbar"
 import BasicCard from "./card1"
 import BasicCard2 from "./card2"
 import MyComponent from "./apicall"
+import FavoriteColor from "./apicall"
 
 const Home = () => {
-  var x = 4
     return (
       <div className="Home">
         {/* <h1>hello {x}</h1> */}
@@ -13,7 +13,7 @@ const Home = () => {
         <br></br>
         <BasicCard2></BasicCard2>
         <br></br>
-        <MyComponent></MyComponent>
+        <FavoriteColor></FavoriteColor>
       </div>
 
 
--- azania_webapp/my-app/src/components/apicall.js
+++ azania_webapp/my-app/src/components/apicall.js
@@ -1,60 +1,28 @@
-import React, { useEffect } from 'react'
-import {useState} from 'react';
+import { useEffect, useState } from "react";
+import Axios from "axios"
+
+function FavoriteColor() {
+  const [getData, setData] = useState(null)
 
 
-function MyComponent() {
-    const [error, setError] = useState(null);
-    const [isLoaded, setIsLoaded] = useState(false);
-    const [items, setItems] = useState([]);
-    const options = {
-        method: 'GET',
-        headers: {
-            'X-RapidAPI-Key': '2c900475e6msh323d593c0c473f4p1bf21cjsnf8d9d965070d',
-            'X-RapidAPI-Host': 'address-from-to-latitude-longitude.p.rapidapi.com'
-        }
-    };
-    
-  
-    // Note: the empty deps array [] means
-    // this useEffect will run once
-    // similar to componentDidMount()
-    useEffect(() => {
-      fetch("https://address-from-to-latitude-longitude.p.rapidapi.com/geolocationapi?address=Pretoria",options)
-        .then(res => res.json())
-        .then(
-          (result) => {
-            // console.log("yes")
-            // setIsLoaded(true);
-            // setItems(result);
-            // alert("yes")
-            alert(JSON.stringify(result))
-            
-          },
-          // Note: it's important to handle errors here
-          // instead of a catch() block so that we don't swallow
-          // exceptions from actual bugs in components.
-          (error) => {
-            setIsLoaded(true);
-            setError(error);
-          }
-        )
-    }, [])
-  
-    if (error) {
-      return <div>Error: {error.message}</div>;
-    } else if (!isLoaded) {
-      return <div>Loading...</div>;
-    } else {
-      return (
-        <ul>
-          {items.map(item => (
-            <li key={item.id}>
-              {item.name} {item.price}
-            </li>
-          ))}
-        </ul>
-      );
+  useEffect(() => {
+    const fetchData = async ()=>{
+
+      const response = await fetch('http://localhost:4000/api/workouts')
+      const json = await response.json()
+      
+      if (response.ok){
+        setData(json)
+
+      }
     }
-  }
+    fetchData()
+  },[])
 
-export default MyComponent
+  return(
+    <h1>yESS</h1>
+  )
+}
+
+
+export default FavoriteColor
