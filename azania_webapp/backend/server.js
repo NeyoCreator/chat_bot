d95967835f3f46c238ed20daf899654b4e53a47c
tsndlov022 <tsndlov022@student.wethinkco.de>
2022-12-11 16:14:48 +0200

    Manged to make the api call by passing the token through the backend

--- azania_webapp/backend/server.js
+++ azania_webapp/backend/server.js
@@ -1,8 +1,27 @@
 const express = require('express')
 const app = express()
 
-app.get("/api", (req,res) => {
-    res.json({"users": ["UserOne","UserTwo","UsertHREE"]})
+app.get("/api", (req, res) => {
+    // res.json({ "users": ["UserOne", "UserTwo", "UsertHREE"] })
+
+    //MAKE API CALL
+    var myHeaders = new Headers();
+    myHeaders.append("token", "52DFB51B-3AAE4C70-9619C792-3C1D99C3");
+    console.log(myHeaders)
+
+    var requestOptions = {
+        method: 'GET',
+        headers: myHeaders,
+        redirect: 'follow'
+    };
+
+    
+    fetch("https://developer.sepush.co.za/business/2.0/area?id=jhbcitypower2-13-marshalltown&test=current", requestOptions)
+        .then(response => response.text())
+        .then(result => res.json(result))
+        .catch(error => console.log('error', error));
+        
+
 })
 
-app.listen(5000, ()=>{console.log("Sever started on port 5000")})
+app.listen(5000, () => { console.log("Sever started on port 5000") })
