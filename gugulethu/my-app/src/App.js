cb7518a81f64d584ae2e0e89da08c9312363aa8c
NeyoCreator <neos25722@gmail.com>
2022-12-26 13:10:21 -0800

    started with send button

--- gugulethu/my-app/src/App.js
+++ gugulethu/my-app/src/App.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import SpeechRecognition, { useSpeechRecognition } from 'react-speech-recognition';
-
+import axios from 'axios';
+// import fetch from 'node-fetch'
+import { fetch } from 'whatwg-fetch';
 
 
 const Dictaphone = () => {
@@ -16,15 +18,32 @@ const Dictaphone = () => {
   }
 
   function getUserData(){
-    
-    const userVoiceData = document.getElementsByTagName('p')[0].innerHTML;
-    console.log(userVoiceData)
 
+    document.getElementById("chatBot").innerHTML = "New text!";
+
+    // let data = {
+    //   voice_message :document.getElementById('userVoice')[0].innerHTML
+    // }
+    // data["voice_message"] = document.getElementById('userVoice')[0].innerHTML;
+    // console.log(JSON.stringify({"voice_message":"yes we have it"}))
+
+    
+    // fetch('/first_call', {
+    //   method: 'POST',
+    //   headers: {
+    //     'Content-Type': 'application/json'
+    //   },
+    //   // body: JSON.stringify(data)
+    // })
+    //   .then(response => response.json())
+    //   .then(data => {
+    //     console.log(data);
+    //   })
+    //   .catch(error => {
+    //     console.error(error);
+    //   });
   }
   
-
-
-
   return (
     <div>
       <center>
@@ -32,13 +51,13 @@ const Dictaphone = () => {
       <p id = "userVoice">{transcript}</p>
       {/* <p>Microphone: {listening ? 'on' : 'off'}</p> */}
       <button onClick={SpeechRecognition.startListening}>Start</button>
-      <button onClick={SpeechRecognition.stopListening}>Send</button>
-      <button onClick={getUserData}>call</button>
-
+      {/* <button onClick={SpeechRecognition.stopListening}>Send</button> */}
+      <button onClick={getUserData}>Send</button>
       {/* <button onClick={resetTranscript}>Reset</button> */}
       
 
       <h1>Gugu :</h1>
+      <p id = "chatBot"></p>
       </center>
 
       
