33d02efdf0cbeac5f0aac24e1ec8cd0757bd192b
tsndlov022 <tsndlov022@student.wethinkco.de>
2023-01-09 16:43:12 +0200

    Pushed final code

--- website/website/src/components/Price.js
+++ website/website/src/components/Price.js
@@ -1,10 +1,15 @@
+import '@stripe/stripe-js';
+
 const Pricepage = () => {
     return(
 
         <div>
         <h1>DDS 1</h1>
         <br></br>
-        <img src="https://i.pinimg.com/564x/8d/48/9c/8d489cb1b6ed7636b3d92bd133f62a64.jpg" alt= ""></img>
+        <img src="https://i.pinimg.com/564x/84/ee/15/84ee15e45c05062e669099dd68955946.jpg" alt= "" width="400" 
+     height="500"></img>
+
+<script src="https://js.stripe.com/v3" async></script>
         </div>
     )
 }
