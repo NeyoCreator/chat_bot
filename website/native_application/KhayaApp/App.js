86eb82c1e52539d44b0bc47def6f901dfe5d733e
tsndlov022 <tsndlov022@student.wethinkco.de>
2023-01-14 17:01:03 +0200

    Restructured the homepage

--- website/native_application/KhayaApp/App.js
+++ website/native_application/KhayaApp/App.js
@@ -1,35 +1,11 @@
 import React, { useState } from 'react';
 import { View, Text, Button } from 'react-native';
-import Display from './components/display';
-import BatteryTitle from './components/titles/batterytitle';
-import BatteryCard from './components/cards/batterycard';
-import LoadSheddingTittle from './components/titles/loadsheddingtitle';
-import LoadSheddingCard from './components/cards/loadsheddingcard';
+import HomePageDisplay from './homepage/hompagedisplay';
 
 const App = () => {
-  // const [showText, setShowText] = useState(true);
 
   return (
-      <View>
-
-      {/* 1.BATTERY INFORMATION */}
-      <br></br>
-      {/* <BatteryTitle/>
-      <br></br>
-      <BatteryCard/> */}
-
-      <br></br>
-      <br></br>
-
-      {/* 2.LOADSHEDDING DATA */}
-      {/* <LoadSheddingTittle/>
-      <br></br>
-      <LoadSheddingCard/> */}
-
-     <h1>Hello</h1>
-
-
-      </View>
+      <HomePageDisplay/>
     
   );
 };
---
+++ website/native_application/KhayaApp/homepage/hompagedisplay.js
@@ -0,0 +1,30 @@
+import { View, Text, Button } from 'react-native';
+import BatteryTitle from './titles/batterytitle';
+import LoadSheddingTittle from './titles/loadsheddingtitle';
+import BatteryCard from './cards/batterycard';
+import LoadSheddingCard from './cards/loadsheddingcard';
+
+
+
+const HomePageDisplay = () => {
+    return (
+        <View>
+            {/* 1.BATTERY INFORMATION */}
+            <br></br>
+            <BatteryTitle />
+            <br></br>
+            <BatteryCard />
+
+            <br></br>
+            <br></br>
+
+            {/* 2.LOADSHEDDING DATA */}
+            <LoadSheddingTittle />
+            <br></br>
+            <LoadSheddingCard />
+        </View>
+
+    );
+};
+
+export default HomePageDisplay;
