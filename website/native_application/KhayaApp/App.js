26b92da941f64bd014bb8fbd03fea9d63793bf7b
tsndlov022 <tsndlov022@student.wethinkco.de>
2023-01-14 13:03:29 +0200

    Added Display component

--- website/native_application/KhayaApp/App.js
+++ website/native_application/KhayaApp/App.js
@@ -1,13 +1,13 @@
 import React, { useState } from 'react';
 import { View, Text, Button } from 'react-native';
+import Display from './components/display';
 
 const App = () => {
-  const [showText, setShowText] = useState(true);
+  // const [showText, setShowText] = useState(true);
 
   return (
     <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-      {showText && <Text>Hello World</Text>}
-      <Button title="Toggle Text" onPress={() => setShowText(!showText)} />
+      <Display/>
     </View>
   );
 };
---
+++ website/native_application/KhayaApp/components/display.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const Display = () => {
+    return (
+        <h1>Hello One man</h1>
+    );
+};
+
+export default Display;
